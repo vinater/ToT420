8ef74a240e4a7ed38a3276e8eef3784406e8eb64
vinater <vinater1@gmail.com>
2013-05-16 15:33:49 +0000

    just testing creating a file and small code

---
+++ HelloWorld.cpp
@@ -0,0 +1,37 @@
+//
+//
+//
+//
+//    Bara ad prufa sma
+//
+//
+//
+//    Herna vaeri gaman ad sja sma koda
+//
+//
+//
+//
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+  cout << "HELLO WORLD" << endl;
+	system("pause");
+
+	return 0;
+}
+
+//
+//
+//
+
+
